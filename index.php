303b92b50ce91af1c5f6140e055c8ee3b0ed5dfb
unknown <dylanbourbotte.simplon@gmail.comgit config --global user.name Dylan>
2019-03-19 09:02:26 +0100

    ajout du formulaire d'inscription

--- index.php
+++ index.php
@@ -10,15 +10,25 @@ require('config.php')
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inscription</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-
+<div class='container'>
+<div class='flex'>
 <form method='post' action=''>
     <label for="pseudo">Identifiant:</label>
     <input type="text" placeholder='Votre identifiant' id='pseudo' name='pseudo'>
+    <label for="mail">Mail:</label>
+    <input type="text" placeholder='Votre adresse email' id='mail' name='mail'>
+    <label for="mail2">Confirmation du mail:</label>
+    <input type="text" placeholder='Votre adresse email' id='mail2' name='mail2'>
+    <label for="mdp">Mot de passe:</label>
+    <input type="password" id='mdp' name='mdp'>
+    <label for="mdp2">Confirmation du mot de passe:</label>
+    <input type="password" id='mdp2' name='mdp2'>
 </form>
-
+</div>
+</div>
 </body>
 </html>
-
 
