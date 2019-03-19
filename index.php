5b4ebc240c38a353a4f9db5bc204c819497bfcc9
unknown <dylanbourbotte.simplon@gmail.comgit config --global user.name Dylan>
2019-03-19 08:28:30 +0100

    ajout des fichiers

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+require('config.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<form method='post' action=''>
+    <label for="pseudo">Identifiant:</label>
+    <input type="text" placeholder='Votre identifiant' id='pseudo' name='pseudo'>
+</form>
+
+</body>
+</html>
+
+
